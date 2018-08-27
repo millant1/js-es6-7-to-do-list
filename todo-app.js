a5c445f4e8a3ea361f58ef2ade42b7ef370dbaf7
Tom Millan <tmillan7997@gmail.com>
2018-08-27 16:24:00 -0400

    Add files via upload

--- todo-app.js
+++ todo-app.js
@@ -17,26 +17,47 @@ const todos = [{
 }
 ]
 
-const incomplete = todos.filter(function (todo){
-    return !todo.completed
-})
-const summary = document.createElement('h2')
-summary.textContent = `you have ${incomplete.length} todos left`
-document.querySelector('body').appendChild(summary)
 
-todos.forEach(function (todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+const filters = {
+    searchText: ''
+}
 
-// Listen for new todo creation
+const renderedTodos = function (todos, filters){
+    const filteredTodos = todos.filter(function (todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-document.querySelector('#add-todo').addEventListener('click', function (e){
+    const incomplete = filteredTodos.filter(function (todo){
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+    
+    const summary = document.createElement('h2')
+    summary.textContent = `you have ${incomplete.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+    
+    filteredTodos.forEach(function (todo){
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+    
+}
+
+renderedTodos(todos, filters)
+
+ // Listen for new todo creation
+ document.querySelector('#add-todo').addEventListener('click', function (e){
     console.log('Add a new todo')
 })
 
 //listen for todo text change
 document.querySelector('#new-todo').addEventListener('input', function (e){
-    console.log(e.target.value)
+   console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e){
+    filters.searchText = e.target.value
+    renderedTodos(todos, filters)
 })
